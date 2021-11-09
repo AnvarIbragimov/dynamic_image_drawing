d3e68222d4c1602e566d2ef1c2163036be24326c
1 <anvaribragimov1996@gmail.com>
2021-11-09 17:34:22 +0500

    commit

---
+++ app/src/main/java/mobile/uz/dynamic_image_drawing/MainActivity.kt
@@ -0,0 +1,11 @@
+package mobile.uz.dynamic_image_drawing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
