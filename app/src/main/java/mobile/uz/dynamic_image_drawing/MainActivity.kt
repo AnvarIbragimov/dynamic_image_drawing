6b166d34d35e326eefb9d71431e2bbf263e13126
1 <anvaribragimov1996@gmail.com>
2021-11-16 10:55:30 +0500

    Algorithm optimization

--- app/src/main/java/mobile/uz/dynamic_image_drawing/MainActivity.kt
+++ app/src/main/java/mobile/uz/dynamic_image_drawing/MainActivity.kt
@@ -2,41 +2,24 @@ package mobile.uz.dynamic_image_drawing
 
 import android.annotation.SuppressLint
 import android.app.AlertDialog
-import android.graphics.Bitmap
 import android.graphics.Color
-import android.graphics.Point
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.MotionEvent
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-
-    companion object {
-
-        private val algorithms = arrayListOf("Recursion")
-
-    }
-
-    private var bitmap1: Bitmap? = null
-    private var bitmap2: Bitmap? = null
-    private var animationTime = 200L
 
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.bt_size -> {
+                // iv_1 equal to iv_2
                 showImageSize(iv_1)
             }
             R.id.bt_generate -> {
-                bitmap1 = bitmapMatrix(iv_1)
-                iv_1.setImageBitmap(bitmap1)
-                bitmap2 = bitmapMatrix(iv_2)
-                iv_2.setImageBitmap(bitmap2)
+                iv_1.generateBitmap()
+                iv_2.generateBitmap()
             }
         }
     }
@@ -45,16 +28,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initBasics()
         initListeners()
         initSpinnersItems()
     }
 
-    private fun initSpinnersItems() {
-        sp_1.setItems(algorithms)
-        sp_2.setItems(algorithms)
+    private fun initBasics() {
+        iv_1.setSquareSize(20)
+        iv_1.setReplaceColor(Color.RED)
+        iv_2.setSquareSize(30)
+        iv_2.setReplaceColor(Color.YELLOW)
     }
 
-    @SuppressLint("ClickableViewAccessibility", "SetTextI18n")
+    private fun initSpinnersItems() {
+        val al = iv_1.getAlgorithms()
+        sp_1.setItems(al)
+        sp_2.setItems(al)
+    }
+
+    @SuppressLint("SetTextI18n")
     private fun initListeners() {
         bt_size.setOnClickListener(this)
         bt_generate.setOnClickListener(this)
@@ -67,97 +59,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         speed_seekbar.onProgressChanged { endPoint ->
             endPoint?.let { process ->
                 tv_speed.text = "${Utils.formatCardBalance(process / 1000.0).toDouble()} s"
-                animationTime = process.toLong()
+                iv_1.setAnimationDuration(process.toLong())
+                iv_2.setAnimationDuration(process.toLong())
             }
-        }
-
-        iv_1.setOnTouchListener { view, event ->
-            when (event?.action) {
-                MotionEvent.ACTION_UP -> {
-                    val point = Point()
-                    event.x.toInt().toString().showInLog()
-                    event.y.toInt().toString().showInLog()
-                    point.x = event.x.toInt()
-                    point.y = event.y.toInt()
-                    bitmap1?.let { b -> fillBlack(iv_1, b, point, Color.WHITE, Color.BLACK) }
-                }
-            }
-            true
-        }
-
-        iv_2.setOnTouchListener { view, event ->
-            when (event?.action) {
-                MotionEvent.ACTION_UP -> {
-                    val point = Point()
-                    event.x.toInt().toString().showInLog()
-                    event.y.toInt().toString().showInLog()
-                    point.x = event.x.toInt()
-                    point.y = event.y.toInt()
-                    bitmap2?.let { b -> fillBlack(iv_2, b, point, Color.WHITE, Color.BLACK) }
-                }
-            }
-            true
-        }
-    }
-
-    private fun fillBlack(imageView: ImageView, bitmap: Bitmap, point: Point, tc: Int, rc: Int) {
-        if (bitmap.width >= point.x && 0 <= point.x && bitmap.height >= point.y && 0 <= point.y) {
-            if (bitmap.getPixel(point.x, point.y) == tc) {
-                bitmap.setPixel(point.x, point.y, rc)
-                val startPX = point.x - point.x % 30
-                val startPY = point.y - point.y % 30
-                val endX = if (startPX + 30 < bitmap.width) startPX + 30 else bitmap.width
-                val endY = if (startPY + 30 < bitmap.height) startPY + 30 else bitmap.height
-                fillSquare(bitmap, startPX, endX, startPY, endY, rc)
-
-                Handler(Looper.getMainLooper()).postDelayed({
-                    if (startPX - 30 >= 0 && startPX - 30 <= bitmap.width)
-                        fillBlack(imageView, bitmap, Point(startPX - 30, point.y), tc, rc)
-                }, animationTime)
-                Handler(Looper.getMainLooper()).postDelayed({
-                    if (startPX + 30 >= 30 && startPX + 30 < bitmap.width)
-                        fillBlack(imageView, bitmap, Point(startPX + 30, point.y), tc, rc)
-                }, animationTime)
-                Handler(Looper.getMainLooper()).postDelayed({
-                    if (startPY - 30 >= 0 && startPY - 30 <= bitmap.height)
-                        fillBlack(imageView, bitmap, Point(point.x, startPY - 30), tc, rc)
-                }, animationTime)
-                Handler(Looper.getMainLooper()).postDelayed({
-                    if (startPY + 30 >= 30 && startPY + 30 < bitmap.height)
-                        fillBlack(imageView, bitmap, Point(point.x, startPY + 30), tc, rc)
-                }, animationTime)
-            }
-            imageView.setImageBitmap(bitmap)
-        }
-    }
-
-    private fun bitmapMatrix(imageView: ImageView): Bitmap {
-        val width = imageView.width
-        val height = imageView.height
-        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        for (i in 0..width step 30) {
-            for (j in 0..height step 30) {
-                val endX = if (i + 30 < width) i + 30 else width
-                val endY = if (j + 30 < height) j + 30 else height
-                val color = if (Random.nextBoolean()) Color.BLACK else Color.WHITE
-                fillSquare(bitmap, i, endX, j, endY, color)
-            }
-
-        }
-        return bitmap
-    }
-
-    private fun fillSquare(
-        bitmap: Bitmap,
-        startPX: Int,
-        endPX: Int,
-        startPY: Int,
-        endPY: Int,
-        color: Int
-    ) {
-//        "sx:$startPX, ex:$endPX, sy:$startPY, ey:$endPY".showInLog()
-        for (i in startPX until endPX) {
-            for (j in startPY until endPY) bitmap.setPixel(i, j, color)
         }
     }
 
--- app/src/main/java/mobile/uz/dynamic_image_drawing/MyImageView.kt
+++ app/src/main/java/mobile/uz/dynamic_image_drawing/MyImageView.kt
@@ -1,69 +1,138 @@
 package mobile.uz.dynamic_image_drawing
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.*
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.Point
+import android.os.Handler
+import android.os.Looper
+import android.util.AttributeSet
 import android.view.MotionEvent
-import android.view.View
+import android.widget.ImageView
+import androidx.annotation.ColorInt
+import kotlin.random.Random
 
-class MyImageView : View {
+@SuppressLint("AppCompatCustomView")
+class MyImageView : ImageView {
 
-    private var paint: Paint = Paint()
-    private var path: Path
-    private var mBitmap: Bitmap? = null
-    private val p1 = Point()
-    private var canvas = Canvas()
+    private var replaceColor: Int = Color.GREEN // green color by default
+    private var type: AlgorithmType = AlgorithmType.Recursion // default fill algorithm
+    private var squareSize: Int = 30 // squareSize * squareSize pixels
+    private var animationDuration: Long = 200 // 0.2 seconds by default
+    private var bitmap: Bitmap? = null // current bitmap
 
-    constructor(context: Context) : super(context) {
-        paint.isAntiAlias = true
-        this.paint.style = Paint.Style.STROKE
-        paint.strokeJoin = Paint.Join.ROUND
-        paint.strokeWidth = 15f
-        mBitmap = BitmapFactory.decodeResource(resources, R.drawable.elephant)
-            .copy(Bitmap.Config.ARGB_8888, true)
-        path = Path()
-    }
-
-    override fun onDraw(canvas: Canvas?) {
-        canvas?.let { cv ->
-            this.canvas = cv
-            this.paint.color = Color.BLUE
-            mBitmap?.let { cv.drawBitmap(it, 0f, 0f, paint) }
-        }
-    }
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         event?.let { e ->
-            when (event.action) {
+            when (e.action) {
                 MotionEvent.ACTION_DOWN -> {
-                    p1.x = e.x.toInt()
-                    p1.y = e.y.toInt()
-                    val sourceColor = mBitmap?.getPixel(e.x.toInt(), e.y.toInt())
-                    val targetColor = paint.color
-                    if (sourceColor != null && mBitmap != null) {
-                        FloodFill1().floodFill(mBitmap!!, p1, sourceColor, targetColor)
+                    val point = Point()
+                    point.x = event.x.toInt()
+                    point.y = event.y.toInt()
+                    bitmap?.let { b ->
+                        when (type) {
+                            AlgorithmType.Recursion ->
+                                if (b.getPixel(point.x, point.y) != replaceColor &&
+                                    b.getPixel(point.x, point.y) != Color.BLACK
+                                ) useRecursion(this, b, point, Color.WHITE, replaceColor)
+                        }
                     }
-                    invalidate()
+                }
+                else -> {
+                    // nothing
                 }
             }
         }
-
         return true
     }
 
-    fun clear() {
-        path.reset()
-        invalidate()
+    fun setReplaceColor(@ColorInt colorId: Int) {
+        replaceColor = colorId
     }
 
-    fun getCurrentPaintColor() = paint.color
-
-    fun changePaintColor(color: Int) {
-        this.paint.color = color
+    fun setAnimationDuration(duration: Long) {
+        if (duration in 200..5000) animationDuration = duration
     }
 
-//    private fun paintThePixel(bp: Bitmap, p: Point, tc: Int, rc: Int) {
-//        if(bp.get)
-//    }
+    fun setSquareSize(size: Int) {
+        if (size in 1..this.width) squareSize = size
+    }
 
+    fun setAlgorithm(type: AlgorithmType) {
+        this.type = type
+    }
 
+    fun getAlgorithms(): ArrayList<String> {
+        val list = ArrayList<String>()
+        list.add(AlgorithmType.Recursion.toString())
+        return list
+    }
+
+    fun generateBitmap() {
+        bitmap = bitmapMatrix(this)
+        this.setImageBitmap(bitmap)
+    }
+
+    private fun bitmapMatrix(imageView: ImageView): Bitmap {
+        val width = imageView.width
+        val height = imageView.height
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        for (i in 0..width step squareSize) {
+            for (j in 0..height step squareSize) {
+                val endX = if (i + squareSize < width) i + squareSize else width
+                val endY = if (j + squareSize < height) j + squareSize else height
+                val color = if (Random.nextBoolean()) Color.BLACK else Color.WHITE
+                fillSquare(bitmap, color, i, endX, j, endY)
+            }
+        }
+        return bitmap
+    }
+
+    private fun fillSquare(
+        bitmap: Bitmap, color: Int,
+        startPX: Int, endPX: Int,
+        startPY: Int, endPY: Int
+    ) {
+        for (i in startPX until endPX)
+            for (j in startPY until endPY) bitmap.setPixel(i, j, color)
+    }
+
+    private fun useRecursion(iv: ImageView, bitmap: Bitmap, point: Point, tc: Int, rc: Int) {
+        if (bitmap.width >= point.x && 0 <= point.x && bitmap.height >= point.y && 0 <= point.y) {
+            if (bitmap.getPixel(point.x, point.y) == tc) {
+                bitmap.setPixel(point.x, point.y, rc)
+                val startPX = point.x - point.x % squareSize
+                val startPY = point.y - point.y % squareSize
+                val endX =
+                    if (startPX + squareSize < bitmap.width) startPX + squareSize else bitmap.width
+                val endY =
+                    if (startPY + squareSize < bitmap.height) startPY + squareSize else bitmap.height
+
+                fillSquare(bitmap, rc, startPX, endX, startPY, endY)
+
+                Handler(Looper.getMainLooper()).postDelayed({
+                    if (startPX - squareSize >= 0 && startPX - squareSize <= bitmap.width)
+                        useRecursion(iv, bitmap, Point(startPX - squareSize, point.y), tc, rc)
+                }, animationDuration)
+                Handler(Looper.getMainLooper()).postDelayed({
+                    if (startPX + squareSize >= squareSize && startPX + squareSize < bitmap.width)
+                        useRecursion(iv, bitmap, Point(startPX + squareSize, point.y), tc, rc)
+                }, animationDuration)
+                Handler(Looper.getMainLooper()).postDelayed({
+                    if (startPY - squareSize >= 0 && startPY - squareSize <= bitmap.height)
+                        useRecursion(iv, bitmap, Point(point.x, startPY - squareSize), tc, rc)
+                }, animationDuration)
+                Handler(Looper.getMainLooper()).postDelayed({
+                    if (startPY + squareSize >= squareSize && startPY + squareSize < bitmap.height)
+                        useRecursion(iv, bitmap, Point(point.x, startPY + squareSize), tc, rc)
+                }, animationDuration)
+            }
+            iv.setImageBitmap(bitmap)
+        }
+    }
+
+    enum class AlgorithmType { Recursion }
 }
